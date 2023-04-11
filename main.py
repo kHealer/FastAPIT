bdcf5244ea89fbfba2ff80130a4c2e3d3fc9cfa6
Safa HELVACI <95585412+kHealer@users.noreply.github.com>
2023-04-11 11:44:16 +0300

    try

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Msg(BaseModel):
 @app.get("/")
 
 async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+    return {"message": "is it still work?"}
 
 
 @app.get("/homepage")
